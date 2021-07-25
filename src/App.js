11a0c5794e222cf0bf51c0c6be0e500313b886a0
Sreemannarayana Ikkurthi <ikkurthis1998@gmail.com>
2021-07-25 07:54:17 +0530

    react-boiler plate, initializing app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+	console.log("mounted");
+	return (
+		<div>
+			<h1>React Template</h1>
+		</div>
+	);
+};
+
+export default App;
